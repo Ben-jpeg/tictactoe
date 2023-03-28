ca75b746f7a2ec1fcc53aa4f606850661949ac75
Bendjade BAROINI <72921912+Ben-jpeg@users.noreply.github.com>
2023-03-28 13:55:16 +0200

    js functions adds

--- script.js
+++ script.js
@@ -1,3 +1,60 @@
 const cells = document.querySelectorAll('[data-cell]');
-const gameStatuts = document.getElementById('gameStatuts')
-const endgameStatuts = document.getElementById('endGameStatuts')
+const gameStatuts = document.getElementById('gameStatuts');
+const endgameStatuts = document.getElementById('endGameStatuts');
+
+const playerOne = 'X';
+const PlayerTwo = 'O';
+
+let playerTurn = playerOne;
+
+cells.forEach(cell => {
+    cell.addEventListener('click', playGame, {once:true});
+});
+
+function playGame(e) {
+    e.target.innerHTML = playerTurn;
+
+    updateGameStatuts(playerTurn);
+
+    playerTurn == playerOne ? playerTurn = PlayerTwo : playerTurn = playerOne;
+};
+
+function updateGameStatuts(statuts) {
+    let statutsText;
+
+    switch (statuts) {
+        case 'X': 
+                 statutsText = "player 2 (O) go !";
+        break;
+
+        case 'O': 
+                 statutsText = "player 1 (X) go !";
+        break;
+
+        case 'winsX': 
+                 statutsText = "player 1 (X) wins ! ";
+        break;
+
+        case 'winsO': 
+                 statutsText = "player 2 (O) wins !";
+        break;
+
+        case 'draw': 
+                 statutsText = "Draw ! Egalité";
+        break;
+
+    }
+
+    gameStatuts.innerHTML = statutsText;
+    endGameStatuts.innerHTML = statutsText;
+}
+
+function endGame() {
+document.getElementById("gameEnd").style.display = "block"
+};
+
+function reloadGame() {
+window.location.reload();
+};
+
+
