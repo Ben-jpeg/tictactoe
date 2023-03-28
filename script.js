350bc7695e12fa80c8cc83e09fdb4ee860c757fa
Bendjade BAROINI <72921912+Ben-jpeg@users.noreply.github.com>
2023-03-28 15:30:09 +0200

    functions completed

--- script.js
+++ script.js
@@ -3,9 +3,23 @@ const gameStatuts = document.getElementById('gameStatuts');
 const endgameStatuts = document.getElementById('endGameStatuts');
 
 const playerOne = 'X';
-const PlayerTwo = 'O';
+const playerTwo = 'O';
 
 let playerTurn = playerOne;
+
+
+const winningPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+]
+
+
 
 cells.forEach(cell => {
     cell.addEventListener('click', playGame, {once:true});
@@ -14,10 +28,36 @@ cells.forEach(cell => {
 function playGame(e) {
     e.target.innerHTML = playerTurn;
 
+    if(checkWin(playerTurn)) {
+              updateGameStatuts("wins" + playerTurn);
+        return endGame();
+
+     } else if (checkDraw()) {
+              updateGameStatuts ("draw");
+        return endGame();
+        }
+    
     updateGameStatuts(playerTurn);
 
-    playerTurn == playerOne ? playerTurn = PlayerTwo : playerTurn = playerOne;
+    playerTurn == playerOne ? playerTurn = playerTwo : playerTurn = playerOne;
 };
+
+
+function checkWin(playerTurn){
+    return winningPatterns.some(combination=> {
+        return combination.every(index => {
+            return cells[index].innerHTML == playerTurn;
+        });
+    });
+};
+
+function checkDraw() {
+    return [...cells].every(cell => {
+        return cell.innerHTML == playerOne || cell.innerHTML == playerTwo;
+    });
+}
+
+
 
 function updateGameStatuts(statuts) {
     let statutsText;
